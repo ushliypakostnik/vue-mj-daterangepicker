3b859b51b6d59d93f46ff1810208d16e63f23ace
levon <levon.gambaryan@gmail.com>
2019-10-10 16:52:22 +0300

    new bundle

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div
     div(style="margin: 20px 0;")
-      date-range-picker(:to="$route.query.to" :from="$route.query.from" locale="fr" submit-title="Actualiser" :panel="$route.query.panel" begin="2016-02-19T00:00:00.000+01:00" @select="checkUpdate" :show-controls="false" :presets="['custom']" :panels=['range'] :theme="theme")
+      date-range-picker(:to="$route.query.to" :from="$route.query.from" locale="ru" submit-title="Actualiser" :panel="$route.query.panel" begin="2016-02-19T00:00:00.000+01:00" @select="checkUpdate" :show-controls="false" :presets="['custom']" :panels="[ 'day', 'week', 'month', 'quarter', 'year']" :theme="theme")
     div(style="margin: 20px 0;")
       date-range-picker(:to="$route.query.to" :from="$route.query.from" @update="checkUpdate" )
     div(style="margin: 20px 0;")
